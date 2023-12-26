af6966c85a80abc6a7b554a8d3086f1191df6e1e
TuanB+UyenH <tuan.bui@eastagile.com+uyen.huynh@eastagile.com>
2023-12-26 10:55:33 +0700

    [r] adjust vite config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [react(), svgr({ exportAsDefault: true })],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          material_ui: ['@mui/material'],
+        },
+      },
+    },
+  },
 });
