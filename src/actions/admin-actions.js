354178f80a59c9e15fe0e4aae3ddcc54d780a803
TuanB+UyenH <>
2023-12-26 15:33:33 +0700

    [r] Integrate admin auth api

--- src/actions/admin-actions.js
+++ src/actions/admin-actions.js
@@ -1,12 +1,16 @@
-import { API_SIGN_IN } from '../constants/api-paths';
-import { SIGN_IN } from '../constants/redux-actions';
+import { API_PASSWORD, API_SIGN_IN } from '../constants/api-paths';
+import { RESET_PASSWORD, SIGN_IN } from '../constants/redux-actions';
 
-import { post } from './axios-request-actions';
+import { post, put } from './axios-request-actions';
 
 const signIn = (data) => post(SIGN_IN, API_SIGN_IN, data);
 
+const resetPassword = (resetPasswordToken, password, passwordConfirmation) =>
+  put(RESET_PASSWORD, API_PASSWORD, { admin: { password, passwordConfirmation, resetPasswordToken } });
+
 const adminActions = {
   signIn,
+  resetPassword,
 };
 
 export default adminActions;
--- src/constants/redux-actions.js
+++ src/constants/redux-actions.js
@@ -2,3 +2,4 @@ export const ADD_LOADING_API = 'ADD_LOADING_API';
 export const CLEAR_LOADED_API = 'CLEAR_LOADED_API';
 export const CLEAR_AUTH_TOKEN = 'CLEAR_AUTH_TOKEN';
 export const SIGN_IN = 'SIGN_IN';
+export const RESET_PASSWORD = 'RESET_PASSWORD';
